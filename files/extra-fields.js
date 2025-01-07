006ba2881ca5ddb7c123d8737a4e8ec5a4861ba9
Tomas Kelečius <sleepwell95@gmail.com>
2025-01-07 12:30:48 +0200

    Adding test code

---
+++ files/extra-fields.js
@@ -0,0 +1,16 @@
+// Initialize extra fields
+window.ec = window.ec || {};
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+// Add a new optional text input 'How should we sign the package?' to shipping address form
+ec.order.extraFields.wrapping_box_signature = {
+    'title': 'How should we sign the package?',
+    'textPlaceholder': 'Package sign',
+    'type': 'text',
+    'tip': 'We will put a label on a box so the recipient knows who it is from',
+    'required': false,
+    'checkoutDisplaySection': 'shipping_address'
+};
+
+window.Ecwid && Ecwid.refreshConfig();
