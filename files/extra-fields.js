225373fce18aed7b40c04a421b5201bcdaf3ff3c
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 12:25:37 +0300

    Putting all the code into one file

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -1,11 +1,22 @@
 // Initialize extra fields
-// import { apiCall } from 'fetch-points.js';
 
 window.ec = window.ec || {};
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn");
+myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
+};
 
-// await apiCall();
+fetch("https://api.multiparcels.com/v1/locations", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+
 // Add a new optional text input 'How should we sign the package?' to shipping address form
 ec.order.extraFields.wrapping_box_signature = {
     'title': 'Select a pickup point',
