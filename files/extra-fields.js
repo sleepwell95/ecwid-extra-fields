6042cdfc820946d08867a354f2788ddb72f222b4
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 15:39:29 +0300

    Sorting fix 2

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -25,7 +25,7 @@ fetch("https://api.multiparcels.com/v1/locations?limit=50", requestOptions)
     // Map API data to options array using only the name
     const options = locations.map((location) => ({
       title: `${location.name} - ${location.address} - ${location.city} - ${location.postal_code}`
-    })).sort((a, b) => b.title.localeCompare(a.title));
+    })).sort((a, b) => a.title.localeCompare(b.title));
 
     // Assign to extra field
     ec.order.extraFields.wrapping_box_signature = {
