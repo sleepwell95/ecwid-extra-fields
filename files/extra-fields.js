a2c4a46c94dee1a5c61ea26aec618b9e3515a6af
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 13:25:48 +0300

    Populating optiions with data

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -1,49 +1,44 @@
-const { API } = require('./public/modules/fetch-points.js');
-
 // Initialize extra fields
 window.ec = window.ec || {};
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
 
+// Set up request headers
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn");
+myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
 
-// const myHeaders = new Headers();
-// myHeaders.append("Authorization", "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn");
-// myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
-// const requestOptions = {
-//   method: "GET",
-//   headers: myHeaders,
-//   redirect: "follow"
-// };
-
-// fetch("https://api.multiparcels.com/v1/locations", requestOptions)
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error(error));
-
-
-function fetchApi() {
-    return API.api();
-  }
-
-ec.order.extraFields.wrapping_box_signature = {
-    'title': 'Select a pickup point',
-    'textPlaceholder': '',
-    'type': 'select',
-    'options':[
-        { 
-          'title': 'Option1'
-        },
-        { 
-            'title': 'Option2'
-        },
-        { 
-            'title': 'Option3'
-        }
-    ],
-    'tip': 'Select a pickup point from the list',
-    'required': false,
-    'checkoutDisplaySection': 'shipping_methods'
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
 };
 
-window.Ecwid && Ecwid.refreshConfig();
-console.log("Hello there!");
+// Fetch data and populate options
+fetch("https://api.multiparcels.com/v1/locations", requestOptions)
+  .then((response) => response.json())
+  .then((data) => {
+    const locations = data.data;
+
+    // Map API data to options array using only the name
+    const options = locations.map((location) => ({
+      title: location.name
+    }));
+
+    // Assign to extra field
+    ec.order.extraFields.wrapping_box_signature = {
+      title: 'Select a pickup point',
+      textPlaceholder: '',
+      type: 'select',
+      options: options,
+      tip: 'Select a pickup point from the list',
+      required: false,
+      checkoutDisplaySection: 'shipping_methods'
+    };
+
+    // Refresh Ecwid config to apply the new fields
+    window.Ecwid && Ecwid.refreshConfig();
+  })
+  .catch((error) => console.error("API error:", error));
+
+console.log("Fetching pickup points...");
