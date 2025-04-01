7c3e49a979d8abca552c55f30e235c6ce53bdc0b
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 11:18:15 +0300

    Adding options to select

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -5,10 +5,20 @@ ec.order.extraFields = ec.order.extraFields || {};
 
 // Add a new optional text input 'How should we sign the package?' to shipping address form
 ec.order.extraFields.wrapping_box_signature = {
-    'title': 'How should we sign the package?',
-    'textPlaceholder': 'Package sign',
+    'title': 'HSelect a pickup point',
+    'textPlaceholder': '',
     'type': 'select',
-    'options':{option1,option2,option3},
+    'options':[
+        { 
+          'title': 'Option1'
+        },
+        { 
+            'title': 'Option2'
+        },
+        { 
+            'title': 'Option3'
+        }
+    ],
     'tip': 'Select a pickup point from the list',
     'required': false,
     'checkoutDisplaySection': 'shipping_methods'
