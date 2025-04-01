dde8ea8935de2f3236cbbf0fd4b8e36ecc3fc6e4
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 12:36:14 +0300

    Testing public modules

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -1,23 +1,28 @@
-// Initialize extra fields
+import { Api } from './public/modules/fetch-points';
 
+// Initialize extra fields
 window.ec = window.ec || {};
 ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
-const myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn");
-myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow"
-};
 
-fetch("https://api.multiparcels.com/v1/locations", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+
+// const myHeaders = new Headers();
+// myHeaders.append("Authorization", "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn");
+// myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
+// const requestOptions = {
+//   method: "GET",
+//   headers: myHeaders,
+//   redirect: "follow"
+// };
+
+// fetch("https://api.multiparcels.com/v1/locations", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
 
 // Add a new optional text input 'How should we sign the package?' to shipping address form
+const result = await Api.fetchApi();
+console.log(result);
 ec.order.extraFields.wrapping_box_signature = {
     'title': 'Select a pickup point',
     'textPlaceholder': '',
