89137e5d9093a81e661662b8603634b3a2595527
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 16:13:37 +0300

    Added TODO list and changes to gitignore

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -43,3 +43,17 @@ fetch("https://api.multiparcels.com/v1/locations?limit=50", requestOptions)
   .catch((error) => console.error("API error:", error));
 
 console.log("Fetching pickup points...");
+
+//TODO: 
+//!For tomorrow. 
+//* 1. Save the selected option "values" in Ecwid so it would be possible to pass  
+//* it in the MS. 
+// 2. Override fields when other shipping option is selected. 
+// 2.1. Test it with different info showing in the options menu. Could be dummy info too. 
+// 3. Think of a better way of testing everything. 
+// At the moment to go through entire pipeline and then be able to test is way too long.  
+// 3.1. Possible to test locally? 
+// 4. How to get Shipping options data from Ecwid/MS? 
+// 4.1. With shipping options data we could override options select to correct one's in options.  
+
+
