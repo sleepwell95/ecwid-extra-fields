adeffd9c07cc2e72f5e5af29ac339b4f8d79bf23
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-01 15:01:20 +0300

    Using testing bearer key

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -5,10 +5,9 @@ ec.order.extraFields = ec.order.extraFields || {};
 
 
 // Set up request headers
-const BEARER_TOKEN = "Bearer __REPLACE_ME__";
+const BEARER_TOKEN = "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn";
 const myHeaders = new Headers();
 myHeaders.append("Authorization", BEARER_TOKEN);
-myHeaders.append("Cookie", "apiato=eyJpdiI6IlhLaU5IbG9IQXREM0c4TVc0TjR5dFE9PSIsInZhbHVlIjoiZ1NNS2VleWxxaG8za3BUUjBXdU0yZCs5bytuWHJCeHprMVFaWVAyS245UHhMd0U5SG82Y2FLcXZvWXhSWWRtZSIsIm1hYyI6IjE4YTM5MTUyN2FjYmYwNDg4OTc3MDg2NGM2MmJiNGVlMWYzM2E3NzljMzM1NGIzNjQxNDcyZTFkNzZiNTdiYjIifQ%3D%3D");
 
 const requestOptions = {
   method: "GET",
