1ac72ba64113a06d6f18b2424d70ba5fada10ad5
Tomas Kelečius <sleepwell95@gmail.com>
2025-04-03 15:10:14 +0300

    Changed default field options

--- files/extra-fields.js
+++ files/extra-fields.js
@@ -1,8 +1,8 @@
 
 // --- CONFIGURATION --- //
 const BEARER_TOKEN = "Bearer InCerbzptEszbxze6xV340gdd8J3FZhn";
-const OMNIVA_METHOD_ID = "8451-1735813681330"; // Replace with your real Omniva ID
-const DPD_METHOD_ID = "46669-1736241676382";    // Replace with your real DPD ID
+const OMNIVA_METHOD_ID = '8451-1735813681330'; // Replace with your real Omniva ID
+const DPD_METHOD_ID = '46669-1736241676382';    // Replace with your real DPD ID
 
 // === SETUP: Declare the field early === //
 ec.order = ec.order || {};
@@ -10,7 +10,9 @@ ec.order.extraFields = ec.order.extraFields || {};
 ec.order.extraFields.pickup_point = {
   title: 'Pasirinkite atsiėmimo tašką',
   type: 'select',
-  options: ["Pasirinkite atsiemimo tašką"], // start empty
+  options: [{
+    'title': 'Pasirinkite atsiemimo tašką'
+  }], // start empty
   required: true,
   tip: 'Pasirinkite terminalą pagal pristatymo metodą',
   checkoutDisplaySection: 'shipping_methods',
